89a8c2817fb858a53e070458bf1d090c4d9c4e28
collero7 <collerortiz@gmail.com>
2020-05-03 12:34:00 +0200

    Updates

---
+++ precache-manifest.6be3fd3cf2a6726da2221ad2a183fc04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6ac7c4b6838fcfc32c58fc735a90de",
+    "url": "/proyecto-react/index.html"
+  },
+  {
+    "revision": "d1db2cbd637f5bd8a9c4",
+    "url": "/proyecto-react/static/css/main.b65f91b4.chunk.css"
+  },
+  {
+    "revision": "874c9525268d95dc1923",
+    "url": "/proyecto-react/static/js/2.55a24465.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/proyecto-react/static/js/2.55a24465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1db2cbd637f5bd8a9c4",
+    "url": "/proyecto-react/static/js/main.b337724f.chunk.js"
+  },
+  {
+    "revision": "44629c7247c9519f50b9",
+    "url": "/proyecto-react/static/js/runtime-main.32fa0c88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proyecto-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
